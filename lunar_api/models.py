89fda87f905ef085895ec4b34e9469da82f32776
Kylan Byrd <aviananalyst@gmail.com>
2020-10-17 21:37:46 -0700

    refactor sqlalchemy models

--- lunar_api/models.py
+++ lunar_api/models.py
@@ -1,29 +1,32 @@
 from sqlalchemy import Column, Integer, DateTime, ForeignKey, String
 from sqlalchemy import Enum as SQLEnum
 from sqlalchemy.orm import relationship
-from sqlalchemy.ext.declarative import declarative_base
-from enum  import Enum as PyEnum
+
+from .database import Base
 
 
-Base = declarative_base()
+class Phase(Base):
+    __tablename__ = 'phases'
 
-
-class Event(PyEnum):
-    new = 1
-    full = 2
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
 
 
 class LunarEvent(Base):
     __tablename__ = 'lunar_event'
+
     id = Column(Integer, primary_key=True)
     cycle = Column(Integer)
-    event = Column(SQLEnum(Event))
     datetime = Column(DateTime)
+    phase_id = Column(Integer, ForeignKey("phases.id"))
+
+    phase = relationship("Phase")
 
 
 class LunarMonth(Base):
     __tablename__ = 'lunar_month'
     id = Column(Integer, primary_key=True)
+    # Should this specifically be the full moon event of the month or something?
     event_id = Column(Integer, ForeignKey(LunarEvent.id))
     event = relationship(LunarEvent)
     month = Column(Integer)
